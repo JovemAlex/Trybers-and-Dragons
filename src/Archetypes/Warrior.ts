a8a7c8188eb87713789960c20c881c685a680ed4
Alex <alexsandrolima991@hotmail.com>
2023-01-18 17:07:24 -0300

    Criação das classes Mage/Necromancer/Ranger/Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energy: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    this._energy = 'stamina';
+    Warrior._createdArchetypeInstances += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._createdArchetypeInstances;
+  }
+}
